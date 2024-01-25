b9db6300f1c7dddb3251f180d731df3782b4c0b2
Priyanka Jagtap <priyanka.jagtap@broadcom.com>
2024-01-25 13:33:04 +0530

    Added jmx connector client

---
+++ jmxconnector/src/main/java/com/vmware/jmxclient/jmxconnector/JmxconnectorApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.jmxclient.jmxconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmxconnectorApplication.class, args);
+	}
+
+}
---
+++ jmxconnector/src/test/java/com/vmware/jmxclient/jmxconnector/JmxconnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.jmxclient.jmxconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmxconnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
