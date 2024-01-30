d98a1e083a5b43483469f6c9ae7ef7ac7fdd20cb
Ankit Girhe <ankit.girhe@broadcom.com>
2024-01-30 13:56:23 +0530

    updated deployment files

--- complete_setup.sh
+++ complete_setup.sh
@@ -1,50 +1,5 @@
 #!/bin/bash
 
-echo "Deploying docker images..."
-
-cd ServiceRegistry
-mvn clean install
-docker build -t demospringapp/serviceregistry:1 .
-kind load docker-image demospringapp/serviceregistry:1
-cd ..
-
-
-cd student
-mvn clean install
-docker build -t demospringapp/student:1 .
-kind load docker-image demospringapp/student:1
-cd ..
-
-
-cd employee
-mvn clean install
-docker build -t demospringapp/employee:1 .
-kind load docker-image demospringapp/employee:1
-cd ..
-
-
-cd messaging
-mvn clean install
-docker build -t demospringapp/messaging:1 .
-kind load docker-image demospringapp/messaging:1
-cd ..
-
-
-cd feignclient
-mvn clean install
-docker build -t demospringapp/feignwebclient:1 .
-kind load docker-image demospringapp/feignwebclient:1
-cd ..
-
-
-cd apigateway
-mvn clean install
-docker build -t demospringapp/demospringapp:1 .
-kind load docker-image demospringapp/demospringapp:1
-cd ..
-
-
-
 echo "Running kubectl commands..."
 
 kubectl apply -f zipkin/deployment.yaml
