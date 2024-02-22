2814a34aafc7b627475f1e9f58542976205ca2b4
Ankit Girhe <ankit.girhe@broadcom.com>
2024-02-22 12:17:49 +0530

    admin server changes

--- admin-server/src/main/java/com/example/springboot/admin/server/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/springboot/admin/server/AdminServerApplication.java
@@ -3,14 +3,8 @@ package com.example.springboot.admin.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 @SpringBootApplication
-@Configuration(proxyBeanMethods = false)
-@EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableAdminServer
 public class AdminServerApplication {
 
